6c6e66fb605310db12e68193ae69db2cafe1d4d3
Amy Bartels <akunrau@gmail.com>
2020-04-08 22:37:50 +0000

    moved setupInput and handleUserInput to its own new file input.js. Exported the setupInput function and require the same function in play.js. Adjusted the export line of client.js to hold and object.

--- client.js
+++ client.js
@@ -46,4 +46,6 @@ const connect = function() {
   return conn;
 }
 
-module.exports = connect;
+module.exports = {
+  connect,
+}
--- play.js
+++ play.js
@@ -1,29 +1,12 @@
 
 const connect = require('./client');
+const setupInput = require('./input');
 
 
 
 console.log('Connecting ...');
 const client = connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", handleUserInput)
-  return stdin;
-}
 
-const handleUserInput = (keyStroke) => {
-  if (keyStroke === '\u0003') {
-    process.exit();
-  }
-  
-}
 
 setupInput();
