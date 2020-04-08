dc2688f262d98c3dda5a05e7c874985510138203
Amy Bartels <akunrau@gmail.com>
2020-04-08 20:48:41 +0000

    moved the connect function from play.js to client.js and made client.js modular

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
-const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '50.64.116.162',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+const connect = require('./client');
 
-  conn.on('connect', () => {
-    console.log('Successfully connected ');
-  });
 
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-    });
-  return conn;
-}
 
 console.log('Connecting ...');
 connect();
